8bca8afc8fd41fc378423e76657d2aad8633c820
qiangbao <qiangbao@hotmail.com>
2015-07-13 17:58:05 -0600

    Create IsPangramModel.cs

---
+++ IsPangramModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pangrams.Models
+{
+    public class IsPangramModel
+    {
+       public bool IsPangram { get; set; }
+    }
+}
