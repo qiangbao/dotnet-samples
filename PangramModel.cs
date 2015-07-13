f3cb0a0a61ea62bb8f345c637048d178a37f0d8a
qiangbao <qiangbao@hotmail.com>
2015-07-13 17:58:34 -0600

    Create PangramModel.cs

---
+++ PangramModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pangrams.Models
+{
+    public class PangramModel
+    {
+        public string Sentence { get; set; }
+
+    }
+}
