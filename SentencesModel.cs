a3b7beacd40ea4ea5df4671fbc9b0adcd074b1c3
qiangbao <qiangbao@hotmail.com>
2015-07-13 17:59:04 -0600

    Create SentencesModel.cs

---
+++ SentencesModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pangrams.Models
+{
+    public class SentencesModel
+    {
+       public  List<string> Pangrams { get; set; }
+    }
+}
